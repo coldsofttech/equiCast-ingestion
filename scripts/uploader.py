47b2afb3a191cfe74dbea333fa1bc76e4f5d917f
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-13 20:51:28 +0100

    Update ingestion.yml (#92)
    * Added AWS Utils
    
    * Updated pipeline.yml
    
    * Update ingestion.yml

--- scripts/uploader.py
+++ scripts/uploader.py
@@ -1,53 +1,35 @@
-import sys
+import argparse
 from pathlib import Path
 
 from equicast_ingestion import UploadConfig, Uploader
 
 
 def main():
-    if len(sys.argv) < 6:
-        print(
-            "Usage: python uploader.py <directory_path> <file_pattern> <custom_message> <s3_bucket> <mode> [s3_prefix]"
-        )
-        print("Modes: generic | stock | fx")
-        print(
-            "Example 1 (Generic): "
-            "python s3_uploader.py ./dist 'ticker_status.json' 'Upload ticker status' my-bucket generic"
-        )
-        print(
-            "Example 2 (Stock):   "
-            "python s3_uploader.py ./stock_downloads '*.parquet' 'Upload stock parquet' my-bucket stock"
-        )
-        print(
-            "Example 3 (FX):      "
-            "python s3_uploader.py ./fx_downloads '*.parquet' 'Upload FX files' my-bucket fx"
-        )
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Upload files to S3 bucket")
+    parser.add_argument("--directory-path", required=True, help="Directory path")
+    parser.add_argument("--file-pattern", required=True, help="File pattern. Example: *.json, *.parquet")
+    parser.add_argument("--custom-message", required=True, help="Custom message")
+    parser.add_argument("--s3-bucket", required=True, help="S3 bucket")
+    parser.add_argument("--mode", required=True, choices=["generic", "fx", "stock"], default="generic", help="Mode")
+    parser.add_argument("--s3-prefix", required=False, default="", help="S3 prefix")
+    args = parser.parse_args()
 
-    dir_path = Path(sys.argv[1])
-    file_pattern = sys.argv[2]
-    custom_message = sys.argv[3]
-    bucket_name = sys.argv[4]
-    mode = sys.argv[5]
-    prefix = sys.argv[6] if len(sys.argv) > 6 else ""
-
+    dir_path = Path(args.directory_path)
     if not dir_path.exists() or not dir_path.is_dir():
         print(f"Error: Path '{dir_path}' does not exist or is not a directory.")
         exit(1)
 
     config = UploadConfig(
         directory=dir_path,
-        pattern=file_pattern,
-        message=custom_message,
-        bucket=bucket_name,
-        mode=mode,
-        prefix=prefix
+        pattern=args.file_pattern,
+        message=args.custom_message,
+        bucket=args.s3_bucket,
+        mode=args.mode,
+        prefix=args.s3_prefix
     )
 
     uploader = Uploader(config=config)
     uploader.upload()
-    uploader.write_summary()
-    uploader.write_outputs()
 
 
 if __name__ == "__main__":
